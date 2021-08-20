f020c089fe0bcbcc9b14f28878938e874cd0e86c
Pham Thanh Quang <quang20201@gmail.com>
2021-08-20 16:58:46 -0700

    create sub_category page

--- app/Http/Controllers/Backend/SubCategoryController.php
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 
 class SubCategoryController extends Controller
 {
     public function SubCategoryView() {
-
+        $subCategories = SubCategory::latest()->get();
+        return view('backend.category.subcategory_view', compact('subCategories'));
     }
 }
--- resources/views/backend/category/subcategory_view.blade.php
+++ resources/views/backend/category/subcategory_view.blade.php
@@ -8,7 +8,7 @@
                 <div class="col-8">
                     <div class="box">
                         <div class="box-header with-border">
-                            <h3 class="box-title">Category List</h3>
+                            <h3 class="box-title">SubCategory List</h3>
                         </div>
                         <!-- /.box-header -->
                         <div class="box-body">
@@ -16,18 +16,18 @@
                                 <table id="example1" class="table table-bordered table-striped">
                                     <thead>
                                     <tr>
-                                        <th>Category Icon</th>
-                                        <th>Category English</th>
-                                        <th>Category Vietnamese</th>
+                                        <th>Category </th>
+                                        <th>SubCategory English</th>
+                                        <th>SubCategory Vietnamese</th>
                                         <th>Action</th>
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    @foreach($categories as $item)
+                                    @foreach($subCategories as $item)
                                         <tr>
-                                            <td> <span><i class="{{ $item->category_icon }}"></i></span></td>
-                                            <td>{{ $item->category_name_en }}</td>
-                                            <td>{{ $item->category_name_vn }}</td>
+                                            <td> <span><i class="{{ $item->category_id }}"></i></span></td>
+                                            <td>{{ $item->subcategory_name_en }}</td>
+                                            <td>{{ $item->subcategory_name_vn }}</td>
                                             <td>
                                                 <a href="{{ route('category.edit', $item->id) }}" class="btn btn-info" title="Edit Data"><i class="fa fa-pencil"></i></a>
                                                 <a href="{{ route('category.delete', $item->id) }}" class="btn btn-danger" title="Delete Data" id="delete"><i class="fa fa-trash"></i></a>
@@ -48,7 +48,7 @@
                 <div class="col-4">
                     <div class="box">
                         <div class="box-header with-border">
-                            <h3 class="box-title">Add Category</h3>
+                            <h3 class="box-title">Add SubCategory</h3>
                         </div>
                         <!-- /.box-header -->
                         <div class="box-body">
@@ -56,16 +56,20 @@
                                 <form action="{{ route('category.store') }}" method="POST">
                                     @csrf
                                     <div class="form-group">
-                                        <h5>Category English<span class="text-danger">*</span></h5>
+                                        <h5>Category Select <span class="text-danger">*</span></h5>
                                         <div class="controls">
-                                            <input type="text" name="category_name_en" class="form-control">
-                                            @error('category_name_en')
-                                            <span class="text-danger">{{ $message }}</span>
-                                            @enderror
+                                            <select name="select" id="select" required="" class="form-control" aria-invalid="false">
+                                                <option value="">Select Category</option>
+                                                <option value="1">India</option>
+                                                <option value="2">USA</option>
+                                                <option value="3">UK</option>
+                                                <option value="4">Canada</option>
+                                                <option value="5">Dubai</option>
+                                            </select>
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <h5>Category Vietnamese <span class="text-danger">*</span></h5>
+                                        <h5>SubCategory English <span class="text-danger">*</span></h5>
                                         <div class="controls">
                                             <input type="text" name="category_name_vn" class="form-control">
                                             @error('category_name_vn')
@@ -73,7 +77,7 @@
                                             @enderror
                                         </div>
                                     </div><div class="form-group">
-                                        <h5>Category Icon<span class="text-danger">*</span></h5>
+                                        <h5>SubCategory Vietnamese<span class="text-danger">*</span></h5>
                                         <div class="controls">
                                             <input type="text" name="category_icon" class="form-control">
                                             @error('category_icon')

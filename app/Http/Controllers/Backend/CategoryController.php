9f2ec75ddd8e8a3974b5fea4bd4a7f7a2eae2a9c
Pham Thanh Quang <quang20201@gmail.com>
2021-08-20 15:51:28 -0700

    create category page

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function CategoryView() {
-
+        $categories = Category::latest()->get();
+        return view('backend.category.category_view', compact('categories'));
     }
 
 }

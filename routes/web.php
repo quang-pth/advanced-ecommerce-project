ce9c279ba1d3058a61fc57dc110e4c4673da595e
Pham Thanh Quang <quang20201@gmail.com>
2021-08-20 16:47:34 -0700

    add sub_categories table

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\AdminProfileController;
 use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Frontend\IndexController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -72,6 +73,12 @@ Route::prefix('category')->group(function() {
     Route::post('/update/{id}', [CategoryController::class, 'CategoryUpdate'])->name('category.update');
     Route::get('/delete/{id}', [CategoryController::class, 'CategoryDelete'])->name('category.delete');
 
+//    Admin Subcategory All Routes
+    Route::get('/sub/view', [SubCategoryController::class, 'SubCategoryView'])->name('all.subcategory');
+    Route::post('/store', [SubCategoryController::class, 'CategoryStore'])->name('category.store');
+    Route::get('/edit/{id}', [SubCategoryController::class, 'CategoryEdit'])->name('category.edit');
+    Route::post('/update/{id}', [SubCategoryController::class, 'CategoryUpdate'])->name('category.update');
+    Route::get('/delete/{id}', [SubCategoryController::class, 'CategoryDelete'])->name('category.delete');
 
 
 });

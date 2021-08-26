9e7c5d32960001dfa0818f4b4ebdf3d0d58a5eb6
Pham Thanh Quang <quang20201@gmail.com>
2021-08-26 16:05:06 -0700

    add onchange on img

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -249,11 +249,12 @@
                                             <div class="form-group">
                                                 <h5>Main Thumbnail<span class="text-danger">*</span></h5>
                                                 <div class="controls">
-                                                    <input type="file" name="product_thumbnail" class="form-control">
+                                                    <input type="file" name="product_thumbnail" class="form-control" onChange="mainThumbnailUrl(this)">
                                                 </div>
                                                 @error('product_thumbnail')
                                                 <span class="text-danger">{{ $message }}</span>
                                                 @enderror
+                                                <img src="" id="mainThumb" alt="">
                                             </div>
                                         </div>
                                         <div class="col-md-4">
@@ -408,5 +409,19 @@
         });
     });
 </script>
+
+<script type="text/javascript">
+    function mainThumbnailUrl(inputImg) {
+        if(inputImg.files && inputImg.files[0]) {
+            const reader = new FileReader();
+            reader.onload = function(ele) {
+                $('#mainThumb').attr('src', ele.target.result).width(80).height(80);
+            };
+            reader.readAsDataURL(inputImg.files[0]);
+        }
+    }
+
+</script>
+
 
 @endsection

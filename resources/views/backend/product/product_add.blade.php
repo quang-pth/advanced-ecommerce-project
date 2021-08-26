93726bdf71c524200d498dcc47b30ae4e773dd6b
Pham Thanh Quang <quang20201@gmail.com>
2021-08-26 15:55:02 -0700

    finish add product page

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <div class="container-full">
     <!-- Content Header (Page header) -->
     <div class="content-header">
@@ -365,6 +366,47 @@
     </section>
     <!-- /.content -->
 </div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('select[name="category_id"]').on('change', function(){
+            const category_id = $(this).val();
+            if(category_id) {
+                $.ajax({
+                    url: "{{  url('/category/subcategory/ajax') }}/"+category_id,
+                    type:"GET",
+                    dataType:"json",
+                    success:function(data) {
+                        $('select[name="subsubcategory_id"]').html('');
+                        const d =$('select[name="subcategory_id"]').empty();
+                        $.each(data, function(key, value){
+                            $('select[name="subcategory_id"]').append('<option value="'+ value.id +'">' + value.subcategory_name_en + '</option>');
+                        });
+                    },
+                });
+            } else {
+                alert('danger');
+            }
+        });
 
+        $('select[name="subcategory_id"]').on('change', function(){
+            const subcategory_id = $(this).val();
+            if(subcategory_id) {
+                $.ajax({
+                    url: "{{  url('category/sub-subcategory/ajax') }}/"+subcategory_id,
+                    type:"GET",
+                    dataType:"json",
+                    success:function(data) {
+                        const d = $('select[name="subsubcategory_id"]').empty();
+                        $.each(data, function(key, value){
+                            $('select[name="subsubcategory_id"]').append('<option value="'+ value.id +'">' + value.subsubcategory_name_en +'</option>');
+                        });
+                    },
+                });
+            } else {
+                alert('danger');
+            }
+        });
+    });
+</script>
 
 @endsection

b61e4fd59b4f206963433cad71a36b94cdb10c3a
Pham Thanh Quang <quang20201@gmail.com>
2021-09-08 14:13:53 -0700

    connect slider to backend

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Slider;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,7 +14,9 @@ class IndexController extends Controller
 {
     public function index() {
         $categories = Category::orderBy('category_name_en', 'ASC')->get();
-        return view('frontend.index', compact('categories'));
+        define('LIMIT_SLIDERS', 3);
+        $sliders = Slider::where('status', '=', 1)->orderBy('id', 'DESC')->limit(LIMIT_SLIDERS)->get();
+        return view('frontend.index', compact('categories', 'sliders'));
     }
 
     public function UserLogout() {
